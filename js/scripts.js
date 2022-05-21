cce57f09a7a9e7258b383e4f402e432a5e0fca68
cheito70 <49110712+cheito70@users.noreply.github.com>
2022-05-21 13:34:12 -0500

    Created css, Index and .js files
    Created css, index, and .js files

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+let myName = 'Jose';
+let joseSays = 'What up bitches! Welcome to my Site!';
+document.write(myName);
+myName = 'Cheito';
+document.write(myName);
+alert(joseSays);
+
+//Below are examples of operators
+/*let simpleAddition = 2 + 2;
+document.write(simpleAddition);*/
+
+let size = 100;
+let doubleSize = size * 2;
+let minSize = (doubleSize * 2) - (size / 2);
+document.write(minSize);
