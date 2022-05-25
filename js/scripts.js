d3c5524faf3c9a45dc592e9d61d2bfb04a6928a7
cheito70 <49110712+cheito70@users.noreply.github.com>
2022-05-24 22:53:26 -0500

    Updated js file with first task
    Created the pokemonList objects within an array. Some pokemon's required an array for their "type".

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,14 @@
-let myName = 'Jose';
-let joseSays = 'Hello World! Welcome to my Site!';
-alert(joseSays);
-
-favoriteFood = 'Mango';
-document.write(favoriteFood);
+/*Initial task of setting up the pokemonList objects. Used
+reference from pokemon.com pokedex for data.*/
+let pokemonlist = [{name: "Charizard", height: "5' 7\"", type: ["Fire", "Flying"]},
+{name: "Wartortle", height: "3' 3\"", type: "Water"},
+{name: "Butterfree", height: "3' 7\"", type: ["Bug", "Flying"]},
+{name: "Zubat", height: "2' 7\"", type: ["Poison", "Flying"]},
+{name: "Machop", height: "2' 7\"", type: "Fighting"},
+{name: "Venusau", height: "6' 7\"", type: ["Grass", "Poison"]},
+{name: "Charmeleon", height: "3' 7\"", type: "Fire"},
+{name: "Drowzee", height: "3' 3\"", type: "Psychic"},
+{name: "Gengar", height: "4' 11\"", type: ["Ghost", "Poison"]},
+{name: "Gyarados", height: "21' 4\"", type: ["Water", "Flying"]},
+{name: "Dragonite", height: "7' 3\"", type: ["Dragon", "Flying"]}
+]
