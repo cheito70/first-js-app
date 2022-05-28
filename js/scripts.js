51a16fcf082965cc1d91e472f85e7bc7e38c7970
cheito70 <49110712+cheito70@users.noreply.github.com>
2022-05-28 10:35:13 -0500

    Added note about conditional

--- js/scripts.js
+++ js/scripts.js
@@ -16,6 +16,7 @@ let pokemonList = [
 for (let i = 0; i < pokemonList.length; i++){
       document.write(pokemonList[i].name + " " + " height: " + pokemonList[i].height + "<br>");
 }
+//Cannot get the conditional to work
 let condition = pokemonList.height > 6;
 if(condition){
   document.write(pokemonList[i].name + ' Wow, that is Huge!');
