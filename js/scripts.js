30c8b6cae2d128cf3ba2a8d515b4073827ee450b
cheito70 <49110712+cheito70@users.noreply.github.com>
2022-06-28 19:07:42 -0500

    loadList function coded
    Included the fetch function along with the chained callback functions .then and .catch.

--- js/scripts.js
+++ js/scripts.js
@@ -34,7 +34,8 @@ function add(pokemon) {
   });
   }
 
-//This function calls the showDetails function when button is clicked
+/*This function calls the showDetails function when button is clicked
+It was incorporated into the "function addListItem" block above
 function eventListener(button, pokemon) {
   button.addEventListener("click", function(){
       showDetails(pokemon);
@@ -43,8 +44,27 @@ function eventListener(button, pokemon) {
 
 function showDetails(pokemon) {
   console.log(pokemon);
-}
+}*/
 
+//This function (loadList) fetches the data in json form from the api
+//and parses it. It contains the response param, the forEach loop function
+//as well as the .then and .catch callback functions.
+function loadList() {
+    return fetch(apiUrl).then(function(response) {
+      return response.json();
+    }).then(function(json) {
+      json.results.forEach(function(item) {
+        let pokemon = {
+          name: item.name,
+          detailsUrl: item.url
+        };
+        add(pokemon);
+        console.log(pokemon);
+      });
+    }).catch (function(e) {
+      console.error(e);
+    });
+  }
 
   return {
     add: add,
